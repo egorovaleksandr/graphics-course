b58fac1f91b62394aa2c685885dd0b9a97a0a3f7
Александр Егоров <yegorov.aleksandr03bk@gmail.com>
2024-11-06 20:27:17 +0300

    hpp

--- tasks/inflight_frames/App.hpp
+++ tasks/inflight_frames/App.hpp
@@ -1,18 +1,15 @@
 #pragma once
 
 #include <etna/Window.hpp>
+#include "wsi/OsWindowingManager.hpp"
 #include <etna/PerFrameCmdMgr.hpp>
 #include <etna/ComputePipeline.hpp>
-#include <etna/GraphicsPipeline.hpp>
-#include <etna/Image.hpp>
-
-#include "wsi/OsWindowingManager.hpp"
-
-#include <chrono>
 #include <etna/Sampler.hpp>
 #include <etna/Buffer.hpp>
+#include <etna/GraphicsPipeline.hpp>
+#include <etna/Image.hpp>
 #include <etna/BlockingTransferHelper.hpp>
-
+#include <chrono>
 
 class App
 {
@@ -34,20 +31,15 @@ private:
 
   std::unique_ptr<etna::Window> vkWindow;
   std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
-
   etna::ComputePipeline texturePipeline;
-  etna::Image textureImage;
-  etna::Sampler textureSampler;
-
   std::unique_ptr<etna::BlockingTransferHelper> transferHelper;
   etna::Image fileTextureImage;
+  etna::Image textureImage;
+  etna::Sampler textureSampler;
   etna::Sampler fileTextureSampler;
-
   etna::GraphicsPipeline shaderPipeline;
-
   std::vector<etna::Buffer> constantsBuffers;
   unsigned cur_frame;
-
   std::chrono::system_clock::time_point timeStart;
 };
 
